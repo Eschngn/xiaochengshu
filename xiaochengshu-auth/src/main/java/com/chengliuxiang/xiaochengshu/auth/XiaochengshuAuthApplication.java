3b727f7dd7072f4ff601a005d959ce5623b26b29
Summer Snow <ScoMorii@163.com>
2025-01-22 22:51:57 +0800

    init

---
+++ xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/XiaochengshuAuthApplication.java
@@ -0,0 +1,13 @@
+package com.chengliuxiang.xiaochengshu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaochengshuAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaochengshuAuthApplication.class, args);
+    }
+
+}
---
+++ xiaochengshu-auth/src/test/java/com/chengliuxiang/xiaochengshu/auth/XiaochengshuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengliuxiang.xiaochengshu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaochengshuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
