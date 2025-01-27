3a5986110cd2ca5000045c673a35b46167ae74e1
Summer Snow <ScoMorii@163.com>
2025-01-27 17:46:07 +0800

    init

--- xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/XiaochengshuAuthApplication.java
+++ xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/XiaochengshuAuthApplication.java
@@ -1,9 +1,11 @@
 package com.chengliuxiang.xiaochengshu.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chengliuxiang.xiaochengshu.auth.domain.mapper")
 public class XiaochengshuAuthApplication {
 
     public static void main(String[] args) {
--- xiaochengshu-auth/src/test/java/com/chengliuxiang/xiaochengshu/auth/XiaochengshuAuthApplicationTests.java
+++ xiaochengshu-auth/src/test/java/com/chengliuxiang/xiaochengshu/auth/XiaochengshuAuthApplicationTests.java
@@ -1,11 +1,13 @@
 package com.chengliuxiang.xiaochengshu.auth;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
 class XiaochengshuAuthApplicationTests {
-
     @Test
     void contextLoads() {
     }
