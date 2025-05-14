7af6c99bbe7e29b6dba97d7bb709debaa5b6b37b
Shawn <ScoMorii@163.com>
2025-05-14 16:10:25 +0800

    业务接口日志

--- xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/controller/TestController.java
+++ xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.chengliuxiang.xiaochengshu.auth.controller;
 
+import com.chengliuxiang.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.chengliuxiang.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/test")
+    @ApiOperationLog(description = "测试接口")
     public Response<String>  test(){
         return Response.success("hello world");
     }
---
+++ xiaochengshu-framework/xiaochengshu-spring-boot-starter-biz-operationlog/src/main/java/com/chengliuxiang/framework/biz/operationlog/config/ApiOperationLogAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.chengliuxiang.framework.biz.operationlog.config;
+
+import com.chengliuxiang.framework.biz.operationlog.aspect.ApiOperationLogAspect;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+public class ApiOperationLogAutoConfiguration {
+    @Bean
+    public ApiOperationLogAspect apiOperationLogAspect() {
+        return new ApiOperationLogAspect();
+    }
+}
