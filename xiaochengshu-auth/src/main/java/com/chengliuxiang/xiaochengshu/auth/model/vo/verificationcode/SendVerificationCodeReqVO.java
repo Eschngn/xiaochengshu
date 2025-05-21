cc1d8d8170940ee3dd977dc902373c70a794b687
Summer Snow <ScoMorii@163.com>
2025-05-21 21:14:29 +0800

    自定义手机号校验注解

--- xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/model/vo/verificationcode/SendVerificationCodeReqVO.java
+++ xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/model/vo/verificationcode/SendVerificationCodeReqVO.java
@@ -1,5 +1,6 @@
 package com.chengliuxiang.xiaochengshu.auth.model.vo.verificationcode;
 
+import com.chengliuxiang.framework.common.validator.PhoneNumber;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,5 +14,6 @@ import lombok.NoArgsConstructor;
 public class SendVerificationCodeReqVO {
 
     @NotBlank(message = "手机号不能为空")
+    @PhoneNumber
     private String phone;
 }
---
+++ xiaochengshu-framework/xiaochengshu-common/src/main/java/com/chengliuxiang/framework/common/validator/PhoneNumberValidator.java
@@ -0,0 +1,16 @@
+package com.chengliuxiang.framework.common.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber,String> {
+    @Override
+    public void initialize(PhoneNumber constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext constraintValidatorContext) {
+        return phoneNumber != null && phoneNumber.matches("\\d{11}");
+    }
+}
