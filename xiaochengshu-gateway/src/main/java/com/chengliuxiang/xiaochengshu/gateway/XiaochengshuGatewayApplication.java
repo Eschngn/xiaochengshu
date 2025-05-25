6e2bd9bc53020d41cee92f8ba9549ad0103cc923
Summer Snow <ScoMorii@163.com>
2025-05-25 16:42:00 +0800

    网关服务搭建

---
+++ xiaochengshu-gateway/src/main/java/com/chengliuxiang/xiaochengshu/gateway/XiaochengshuGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.chengliuxiang.xiaochengshu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaochengshuGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaochengshuGatewayApplication.class, args);
+    }
+}
