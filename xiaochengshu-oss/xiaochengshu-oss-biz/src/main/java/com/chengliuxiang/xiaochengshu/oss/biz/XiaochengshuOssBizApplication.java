1b1a12a198840ef580c1f813570d9805fe41085c
Summer Snow <ScoMorii@163.com>
2025-06-13 20:07:51 +0800

    对象存储服务搭建

---
+++ xiaochengshu-oss/xiaochengshu-oss-biz/src/main/java/com/chengliuxiang/xiaochengshu/oss/biz/XiaochengshuOssBizApplication.java
@@ -0,0 +1,11 @@
+package com.chengliuxiang.xiaochengshu.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaochengshuOssBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaochengshuOssBizApplication.class, args);
+    }
+}
