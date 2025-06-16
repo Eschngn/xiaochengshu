2ad6232309dfa66dc39abfdce8fa4d278406ef92
Shawn <ScoMorii@163.com>
2025-06-16 19:21:23 +0800

    认证服务-登陆注册

---
+++ xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/rpc/UserRpcService.java
@@ -0,0 +1,36 @@
+package com.chengliuxiang.xiaochengshu.auth.rpc;
+
+import com.chengliuxiang.framework.common.response.Response;
+import com.chengliuxiang.xiaochengshu.user.api.UserFeignApi;
+import com.chengliuxiang.xiaochengshu.user.dto.req.FindUserByPhoneReqDTO;
+import com.chengliuxiang.xiaochengshu.user.dto.req.RegisterUserReqDTO;
+import com.chengliuxiang.xiaochengshu.user.dto.resp.FindUserByPhoneRspDTO;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRpcService {
+
+    @Resource
+    private UserFeignApi userFeignApi;
+
+    public Long registerUser(String phone) {
+        RegisterUserReqDTO registerUserReqDTO = new RegisterUserReqDTO();
+        registerUserReqDTO.setPhoneNumber(phone);
+        Response<Long> response = userFeignApi.registerUser(registerUserReqDTO);
+        if (!response.isSuccess()) {
+            return null;
+        }
+        return response.getData();
+    }
+
+    public FindUserByPhoneRspDTO findUserByPhone(String phone) {
+        FindUserByPhoneReqDTO findUserByPhoneReqDTO = new FindUserByPhoneReqDTO();
+        findUserByPhoneReqDTO.setPhone(phone);
+        Response<FindUserByPhoneRspDTO> response = userFeignApi.findByPhone(findUserByPhoneReqDTO);
+        if(!response.isSuccess()){
+            return null;
+        }
+        return response.getData();
+    }
+}
--- xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/service/impl/AuthServiceImpl.java
+++ xiaochengshu-auth/src/main/java/com/chengliuxiang/xiaochengshu/auth/service/impl/AuthServiceImpl.java
@@ -1,16 +1,21 @@
 package com.chengliuxiang.xiaochengshu.auth.service.impl;
 
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
 import com.chengliuxiang.framework.common.exception.BizException;
 import com.chengliuxiang.framework.common.response.Response;
 import com.chengliuxiang.xiaochengshu.auth.constant.RedisKeyConstants;
 import com.chengliuxiang.xiaochengshu.auth.enums.LoginTypeEnum;
 import com.chengliuxiang.xiaochengshu.auth.enums.ResponseCodeEnum;
 import com.chengliuxiang.xiaochengshu.auth.model.vo.user.UserLoginReqVO;
+import com.chengliuxiang.xiaochengshu.auth.rpc.UserRpcService;
 import com.chengliuxiang.xiaochengshu.auth.service.AuthService;
+import com.chengliuxiang.xiaochengshu.user.dto.resp.FindUserByPhoneRspDTO;
 import com.google.common.base.Preconditions;
 import jakarta.annotation.Resource;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -19,6 +24,12 @@ import java.util.Objects;
 public class AuthServiceImpl implements AuthService {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
+    private UserRpcService userRpcService;
+
+    @Resource
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public Response<String> loginAndRegister(UserLoginReqVO userLoginReqVO) {
@@ -30,17 +41,46 @@ public class AuthServiceImpl implements AuthService {
         }
         Long userId = null;
         switch (loginTypeEnum) {
-            case VERIFICATION_CODE:
+            case VERIFICATION_CODE: // 验证码登陆
                 String verificationCode = userLoginReqVO.getCode();
                 Preconditions.checkArgument(StringUtils.isNotBlank(verificationCode), "验证码不能为空");
                 String key = RedisKeyConstants.buildVerificationCodeKey(phone);
                 String sentCode = (String) redisTemplate.opsForValue().get(key);
-                if(!StringUtils.equals(sentCode,verificationCode)){
+                if (!StringUtils.equals(sentCode, verificationCode)) {
                     return Response.fail(ResponseCodeEnum.VERIFICATION_CODE_ERROR);
                 }
-                // TODO: 调用用户服务查询用户是否已注册
+                // 调用用户服务，注册用户
+                Long userIdTmp = userRpcService.registerUser(phone);
+                if (Objects.isNull(userIdTmp)) {
+                    throw new BizException(ResponseCodeEnum.LOGIN_FAIL);
+                }
+                userId = userIdTmp;
+                break;
+            case PASSWORD: // 密码登陆
+                String password = userLoginReqVO.getPassword();
+                FindUserByPhoneRspDTO findUserByPhoneRspDTO = userRpcService.findUserByPhone(phone);
+                if(Objects.isNull(findUserByPhoneRspDTO)){
+                    throw new BizException(ResponseCodeEnum.USER_NOT_FOUND);
+                }
+                // 拿到密文密码
+                String encodePassword = findUserByPhoneRspDTO.getPassword();
+                boolean isPasswordCorrect = passwordEncoder.matches(encodePassword, password);
+                if(!isPasswordCorrect){
+                    throw new BizException(ResponseCodeEnum.PHONE_OR_PASSWORD_ERROR);
+                }
+                userId= findUserByPhoneRspDTO.getId();
+                break;
+            default:
+                break;
         }
 
-        return null;
+        // SaToken 登陆用户，入参为用户 ID
+        StpUtil.login(userId);
+
+        // 获取 Token 令牌
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+
+        // 返回 Token 令牌
+        return Response.success(tokenInfo.tokenValue);
     }
 }
