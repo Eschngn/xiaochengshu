eb753e74165c795afc74937b6568d41b4d808175
Shawn <ScoMorii@163.com>
2025-07-03 12:34:52 +0800

    K-V短文本存储服务搭建

---
+++ xiaochengshu-kv/xiaochengshu-kv-biz/src/main/java/com/chengliuxiang/kv/biz/XiaochengshuKVBizApplication.java
@@ -0,0 +1,11 @@
+package com.chengliuxiang.kv.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaochengshuKVBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaochengshuKVBizApplication.class, args);
+    }
+}
