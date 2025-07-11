c6c6bf7faf3a06c030d8c35d8379915815c4623a
Shawn <ScoMorii@163.com>
2025-07-11 11:23:15 +0800

    K-V短文本存储服务-FeignApi

---
+++ xiaochengshu-kv/xiaochengshu-kv-api/src/main/java/com/chengliuxiang/xiaochengshu/kv/api/KeyValueFeignApi.java
@@ -0,0 +1,26 @@
+package com.chengliuxiang.xiaochengshu.kv.api;
+
+import com.chengliuxiang.framework.common.response.Response;
+import com.chengliuxiang.xiaochengshu.kv.constant.ApiConstants;
+import com.chengliuxiang.xiaochengshu.kv.dto.req.AddNoteContentReqDTO;
+import com.chengliuxiang.xiaochengshu.kv.dto.req.DeleteNoteContentReqDTO;
+import com.chengliuxiang.xiaochengshu.kv.dto.req.FindNoteContentReqDTO;
+import com.chengliuxiang.xiaochengshu.kv.dto.rsp.FindNoteContentRspDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = ApiConstants.SERVICE_NAME)
+public interface KeyValueFeignApi {
+
+    String PREFIX = "/kv";
+
+    @PostMapping(value = PREFIX + "/note/content/add")
+    Response<?> addNoteContent(@RequestBody AddNoteContentReqDTO addNoteContentReqDTO);
+
+    @PostMapping(value = PREFIX + "/note/content/find")
+    Response<FindNoteContentRspDTO> findNoteContent(@RequestBody FindNoteContentReqDTO findNoteContentReqDTO);
+
+    @PostMapping(value = PREFIX + "/note/content/delete")
+    Response<?> deleteNoteContent(@RequestBody DeleteNoteContentReqDTO deleteNoteContentReqDTO);
+}
---
+++ xiaochengshu-kv/xiaochengshu-kv-api/src/main/java/com/chengliuxiang/xiaochengshu/kv/constant/ApiConstants.java
@@ -0,0 +1,6 @@
+package com.chengliuxiang.xiaochengshu.kv.constant;
+
+public interface ApiConstants {
+
+    String SERVICE_NAME = "xiaochengshu-kv";
+}
